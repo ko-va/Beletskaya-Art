85378bdefeeaadd59db002fe9029a0aee3a18ce3
Valentina Kochegarova <kochegarova_v@yahoo.com>
2021-09-22 18:33:18 +0200

    First commit

---
+++ config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  siteTitle: 'Beletskaya Art', // <title>
+  manifestName: 'Beletskaya Art',
+  manifestShortName: 'Art', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/beletskaya-art/`, // This path is subpath of your hosting https://domain/portfolio
+  heading: 'BELETSKAYA',
+  subHeading: 'ART',
+  // social
+  socialLinks: [
+    {
+      icon: 'fa-instagram',
+      name: 'Instagram',
+      url: 'https://www.instagram.com/missbelka',
+    },
+    {
+      icon: 'fa-envelope-o',
+      name: 'Email',
+      url: 'mailto:nadezda.beletskaya@gmail.com',
+    },
+  ],
+};
